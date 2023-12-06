3691d7aaa91e8ba01981fe0c2d570634aef1c295
Illya Moskvin <illya.moskvin@amfam.com>
2023-12-06 01:21:44 -0600

    Generate random number in poc1

--- poc1-api/main.py
+++ poc1-api/main.py
@@ -1,3 +1,6 @@
+import json
+import random
+
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 
@@ -7,9 +10,15 @@ class SecretSauceMaker:
     Substitute this for an ML model, optimization function, etc.
     """
     def make_sauce(self, foo, bar):
-        # TODO: Seeded random number generator here
-        # Should output tabular data: maybe date, revenue?
-        pass
+        dates = [f'2024-{i:02d}-01' for i in range(1, 12)]
+        revenue = 1000000
+        out = []
+        for date in dates:
+            seed = date + str(foo) + str(bar)
+            random.seed(seed)
+            revenue += random.randrange(0, 100000)
+            out.append([int(foo), bar, date, revenue])
+        return out
 
 
 class SauceSaver():
@@ -35,7 +44,6 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         self.sauce_saver = SauceSaver()
         super().__init__(*args, **kwargs)
 
-
     def respond(self, code, message):
         # End body with trailing \n to avoid delays:
         body = str(message) + '\n'
@@ -45,7 +53,6 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         self.wfile.write(str.encode(body))
-
 
     def do_GET(self):
         # Parse query string, keep first value for each key
@@ -61,7 +68,11 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         except Exception:
             return self.respond(500, "Failed to make sauce")
 
-        return self.respond(200, "Made sauce")
+        # Uncomment for debug. This API does not return the result.
+        # It writes the result to a database connected to Looker.
+        return self.respond(200, json.dumps(sauce))
+
+        return self.respond(200, "Made sauce. Please rerun your query.")
 
 
 def serve(handler):
